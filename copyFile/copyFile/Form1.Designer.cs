5935f4bafc1de1009ff18544e1ee5d0b37d2577a
thinhvuthecreator <thinhvuh@gmail.com>
2022-12-01 15:47:52 +0700

    add copy file

---
+++ copyFile/copyFile/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace copyFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.copyBtn = new System.Windows.Forms.Button();
+            this.hostBtn = new System.Windows.Forms.Button();
+            this.sourceBtn = new System.Windows.Forms.Button();
+            this.hostTxtbox = new System.Windows.Forms.TextBox();
+            this.sourceTxtbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.copyBtn);
+            this.groupBox1.Controls.Add(this.hostBtn);
+            this.groupBox1.Controls.Add(this.sourceBtn);
+            this.groupBox1.Controls.Add(this.hostTxtbox);
+            this.groupBox1.Controls.Add(this.sourceTxtbox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(570, 169);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sao chép tập tin";
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.copyBtn.Location = new System.Drawing.Point(157, 105);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(222, 23);
+            this.copyBtn.TabIndex = 6;
+            this.copyBtn.Text = "Sao chép";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            this.copyBtn.Click += new System.EventHandler(this.copyBtn_Click);
+            // 
+            // hostBtn
+            // 
+            this.hostBtn.Location = new System.Drawing.Point(437, 63);
+            this.hostBtn.Name = "hostBtn";
+            this.hostBtn.Size = new System.Drawing.Size(38, 23);
+            this.hostBtn.TabIndex = 5;
+            this.hostBtn.Text = " ...";
+            this.hostBtn.UseVisualStyleBackColor = true;
+            this.hostBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // sourceBtn
+            // 
+            this.sourceBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sourceBtn.Location = new System.Drawing.Point(437, 32);
+            this.sourceBtn.Name = "sourceBtn";
+            this.sourceBtn.Size = new System.Drawing.Size(38, 23);
+            this.sourceBtn.TabIndex = 4;
+            this.sourceBtn.Text = " ...";
+            this.sourceBtn.UseVisualStyleBackColor = true;
+            this.sourceBtn.Click += new System.EventHandler(this.sourceBtn_Click);
+            // 
+            // hostTxtbox
+            // 
+            this.hostTxtbox.Location = new System.Drawing.Point(157, 64);
+            this.hostTxtbox.Name = "hostTxtbox";
+            this.hostTxtbox.Size = new System.Drawing.Size(274, 20);
+            this.hostTxtbox.TabIndex = 3;
+            // 
+            // sourceTxtbox
+            // 
+            this.sourceTxtbox.Location = new System.Drawing.Point(157, 33);
+            this.sourceTxtbox.Name = "sourceTxtbox";
+            this.sourceTxtbox.Size = new System.Drawing.Size(274, 20);
+            this.sourceTxtbox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(17, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Đường dẫn thư mục đích";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(17, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đường dẫn thư mục nguồn";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.groupBox2.Location = new System.Drawing.Point(12, 198);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(570, 56);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tiến trình sao chép";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar1.Location = new System.Drawing.Point(27, 19);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(521, 24);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 279);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button hostBtn;
+        private System.Windows.Forms.Button sourceBtn;
+        private System.Windows.Forms.TextBox hostTxtbox;
+        private System.Windows.Forms.TextBox sourceTxtbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button copyBtn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ copyFile/copyFile/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace copyFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        void copyFile()
+        {
+            DirectoryInfo directoryUri = new DirectoryInfo(sourceTxtbox.Text);
+            var cacFiles = directoryUri.GetFiles();
+            foreach (var file in cacFiles)
+            {
+                File.Copy(file.FullName, hostTxtbox.Text + @"\" + file.Name, true);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFolder = new FolderBrowserDialog();
+            if (openFolder.ShowDialog() == DialogResult.OK)
+            {
+                hostTxtbox.Text = openFolder.SelectedPath.ToString();
+            }
+        }
+
+        private void sourceBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFolder = new FolderBrowserDialog();
+            if(openFolder.ShowDialog() == DialogResult.OK)
+            {
+                sourceTxtbox.Text = openFolder.SelectedPath.ToString();
+            }
+            
+        }
+
+        private void copyBtn_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+      
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Đã hoàn thành !");
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+           
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            copyFile();
+        }
+    }
+}
