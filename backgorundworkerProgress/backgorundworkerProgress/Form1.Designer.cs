ecc5df4b5d9e34bcafa3907376cd16d466eb1c71
thinhvuthecreator <thinhvuh@gmail.com>
2022-12-02 15:22:46 +0700

    da hoan thanh

---
+++ backgorundworkerProgress/backgorundworkerProgress/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace backgorundworkerProgress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.copyBtn = new System.Windows.Forms.Button();
+            this.sourceTxtb = new System.Windows.Forms.TextBox();
+            this.desTxtb = new System.Windows.Forms.TextBox();
+            this.sourceBtn = new System.Windows.Forms.Button();
+            this.desBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(41, 19);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(454, 17);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyBtn.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.copyBtn.Location = new System.Drawing.Point(208, 90);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(156, 23);
+            this.copyBtn.TabIndex = 1;
+            this.copyBtn.Text = "Sao chép";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            this.copyBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sourceTxtb
+            // 
+            this.sourceTxtb.Location = new System.Drawing.Point(155, 35);
+            this.sourceTxtb.Name = "sourceTxtb";
+            this.sourceTxtb.Size = new System.Drawing.Size(255, 20);
+            this.sourceTxtb.TabIndex = 2;
+            // 
+            // desTxtb
+            // 
+            this.desTxtb.Location = new System.Drawing.Point(155, 64);
+            this.desTxtb.Name = "desTxtb";
+            this.desTxtb.Size = new System.Drawing.Size(255, 20);
+            this.desTxtb.TabIndex = 3;
+            // 
+            // sourceBtn
+            // 
+            this.sourceBtn.Location = new System.Drawing.Point(421, 33);
+            this.sourceBtn.Name = "sourceBtn";
+            this.sourceBtn.Size = new System.Drawing.Size(56, 23);
+            this.sourceBtn.TabIndex = 4;
+            this.sourceBtn.Text = "...";
+            this.sourceBtn.UseVisualStyleBackColor = true;
+            this.sourceBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // desBtn
+            // 
+            this.desBtn.Location = new System.Drawing.Point(421, 62);
+            this.desBtn.Name = "desBtn";
+            this.desBtn.Size = new System.Drawing.Size(56, 23);
+            this.desBtn.TabIndex = 5;
+            this.desBtn.Text = "...";
+            this.desBtn.UseVisualStyleBackColor = true;
+            this.desBtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.groupBox1.Location = new System.Drawing.Point(13, 183);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(533, 46);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tiến trình sao chép";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.desBtn);
+            this.groupBox2.Controls.Add(this.copyBtn);
+            this.groupBox2.Controls.Add(this.sourceTxtb);
+            this.groupBox2.Controls.Add(this.sourceBtn);
+            this.groupBox2.Controls.Add(this.desTxtb);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.groupBox2.Location = new System.Drawing.Point(13, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(533, 131);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sao chép tập tin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(15, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Đường dẫn thư mục nguồn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(15, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Đường dẫn thư mục đích";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 259);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button copyBtn;
+        private System.Windows.Forms.TextBox sourceTxtb;
+        private System.Windows.Forms.TextBox desTxtb;
+        private System.Windows.Forms.Button sourceBtn;
+        private System.Windows.Forms.Button desBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ backgorundworkerProgress/backgorundworkerProgress/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace backgorundworkerProgress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        void ham()
+        {
+            DirectoryInfo dr = new DirectoryInfo(sourceTxtb.Text);
+            var cacFiles = dr.GetFiles();
+            int u = 0;
+            if (cacFiles.Length == 0)
+            {
+                MessageBox.Show("Thư mục này không có file nào cả");
+            }
+            else
+            {
+
+                foreach (var file in cacFiles)
+                {
+                    u += 1;
+                    File.Copy(file.FullName, desTxtb.Text + @"\" + file.Name, true);
+                    backgroundWorker1.ReportProgress((int)(u * 100 / cacFiles.Length));
+
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        
+                        label3.Visible = true;
+                        label3.Text = "Đang sao chép " + file.FullName;
+                        toolTip1.SetToolTip(label3, label3.Text);
+                    }));
+                }
+            }
+           
+
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+           
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ham();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Đã sao chép xong !");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+            catch
+            {
+                MessageBox.Show("Tiến trình sao chép bị can thiệp khi đang chạy ! Lỗi !");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            fd.ShowDialog();
+            sourceTxtb.Text = fd.SelectedPath;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            fd.ShowDialog();
+            desTxtb.Text = fd.SelectedPath;
+        }
+
+       
+    }
+}
